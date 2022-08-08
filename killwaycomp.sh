ac82e854b178a1d423e502fe62f6a56270e92938
ahmad <lidgnulinux@gmail.com>
2022-08-08 09:29:49 +0700

    Fix killwaycomp.sh

--- killwaycomp.sh
+++ killwaycomp.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
-if [ pgrep -x "dwl" > /dev/null ]; then
+if pgrep -x "dwl" > /dev/null; then
 	killall dwl
-elif [ pgrep -x "sway" > /dev/null ]; then
+elif pgrep -x "sway" > /dev/null; then
 	killall sway
-elif [ pgrep -x "labwc" > /dev/null ]; then
+elif pgrep -x "labwc" > /dev/null; then
 	killall labwc
-elif [ pgrep -x "hikari" > /dev/null ]; then
+elif pgrep -x "hikari" > /dev/null; then
 	killall hikari
 fi
